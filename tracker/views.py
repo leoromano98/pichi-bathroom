1a9da48eaa0c865e8e737b2b75b18cf0113bbd0e
Leo Romano <59894455+leoromano98@users.noreply.github.com>
2025-12-16 14:06:24 -0300

    add delete btn

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import BathroomVisit
 
 def home(request):
@@ -10,3 +10,8 @@ def home(request):
 
     visits = BathroomVisit.objects.all()
     return render(request, 'tracker/home.html', {'visits': visits})
+
+def delete_visit(request, visit_id):
+    visit = get_object_or_404(BathroomVisit, id=visit_id)
+    visit.delete()
+    return redirect('home')
